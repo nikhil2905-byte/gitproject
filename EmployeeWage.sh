fb10441330b90909e32e9502e2ac1bd0f1167212
unknown <nikhilnakirikanti27@gmail.com>
2020-10-19 16:50:57 +0530

    Wage is added

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -2,10 +2,12 @@
 echo "Welcome to Employee Wage Computation Program"
 isPresent=1;
 randomCheck=$((RANDOM%2));
-if[ $isPresent -eq $randomCheck ]
+if[ $isPresent -eq $randomCheck ];
 then
-		echo "Employee is Present"
+		empRatePerHr=20;
+		empHrs=8;
+		salary=$(($empHrs*$empRatePerHr));
 else
-		echo "Employee is Absent"
+		salary=0;
 fi
 
