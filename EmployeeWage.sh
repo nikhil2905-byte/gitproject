d2faa1691f538627cf1f7dbe26aaada142d960eb
unknown <nikhilnakirikanti27@gmail.com>
2020-10-19 17:19:54 +0530

    Switch case is added

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,17 +1,17 @@
-#!/bin/bash -x
-echo "Welcome to Employee Wage Computation Program"
+#! /bin/bash -x
 isPartTime=1;
 isFullTime=2;
 empRatePerHr=20;
-randomCheck=$(( RANDOM%3 ));
-if[ $isFullTime -eq $randomCheck ];
-then
-		empHrs=8;
-elif[ $isPartTime -eq $randomCheck ];
-then
-		empHrs=4;
-else
-		empHrs=0;
-fi
-salary=$(($empHrs*$empRatePerHr))
-
+empCheck=$(( $RANDOM%3 ));
+case $empCheck in
+	$isFullTime)
+		empHrs=8
+			;;
+	$isPartTime)
+		empHrs=4
+			;;
+		*)
+		empHrs=0
+			;;
+esac
+salary=$(( $empHrs*$empRatePerHr ));
