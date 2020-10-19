048bbc031d0599fd16083aae45c4abf01eba96e0
unknown <nikhilnakirikanti27@gmail.com>
2020-10-19 23:19:59 +0530

    Adding Daily Wages

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -4,7 +4,7 @@ isPartTime=1;
 isFullTime=2;
 maxHrsInMonth=10;
 empRatePerHr=20;
-numWorkingDays=20;
+numWorkingDays=2;
 #VARIABLES
 totalEmpHrs=0;
 totalWorkingDays=0;
@@ -22,9 +22,18 @@ function getWorkingHours() {
 	esac
 	echo $workHours
 }
+function calcDailyWage() {
+	local workHours=$1
+	wages=$(( $workHrs*$empRatePerHr ))
+	echo $wage
+}
 while [[ $totalEmpHrs -lt $maxHrsInMonth && $totalWorkingDays -lt $numWorkingDays ]]
-do	(( totalWorkingDays++ ))
+do
+	(( totalWorkingDays++ ))
 	workHours="$( getWorkingHours $(( $RANDOM%3 )) )"
-	totalWorkHours=$(($totalWorkHours + $workHours))
+	totalWorkHours=$(($totalEmpHours + $workHours))
+	empDailyWage[ "$totalWorkingDays" ]="$( calcDailyWage $totalEmpHours )"
 done
-totalSalary=$(( $totalWorkHours*$empRatePerHr ));
+totalSalary="$(( $totalWorkHours*$calcDailyWage ))"
+echo "daily wage "${empDailyWage[@]}"
+echo "All Keys " ${!empDailyWage[@]}
